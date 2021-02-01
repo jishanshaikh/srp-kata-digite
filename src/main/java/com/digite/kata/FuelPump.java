b822bdbb1895ac78fa3ed1245cd4fdb5c9067d1a
Jishan Shaikh <jshaikh@digite.com>
2021-02-01 11:54:44 +0530

    code for TDD

--- src/main/java/com/digite/kata/FuelPump.java
+++ src/main/java/com/digite/kata/FuelPump.java
@@ -3,6 +3,8 @@ package com.digite.kata;
 public class FuelPump {
 
     public void fillFuel(int amount, Vehicle vehical) {
-        vehical.setRemainingFuel( amount );
+        int fillableAmount = vehical.getMaxFuel() - vehical.getRemainingFuel();
+
+        vehical.setRemainingFuel( amount > fillableAmount ? fillableAmount : vehical.getRemainingFuel() + amount );
     }
 }
